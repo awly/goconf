8cda8581dcdea2d3682794f8cb6e104358132a4c
cron <cron@debcut.vb>
2012-08-03 22:51:26 +0300

    removed unneeded functions, added simple multi-level call for params, cleaned up

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 goconf
 ======
 
-Go library that provides thread-safe json-based configuration manager
+Go library that provides thread-safe json-based configuration provider, with periodic auto-update
+
+go doc github.com/cr0n/goconf ;)
--- conf.go
+++ conf.go
@@ -1,85 +1,76 @@
-package config
+package goconf
 
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"sync"
 	"time"
 )
 
-// Value used as config refresh interval (reading from file) if it was
-// not specified in config.
-const default_refresh_interval = 3
+const default_refresh_interval = 5
 
 var (
-	c     = &config{data: make(map[string]interface{})}
-	cPath = "config.json"
+	c = &config{
+		data: make(map[string]interface{}),
+		path: "config.json",
+	}
 )
+
+type config struct {
+	sync.Mutex
+	data map[string]interface{}
+	path string
+}
 
 func init() {
 	go func() {
 		for {
-			LoadConfig(cPath)
+			LoadConfig(c.path)
 			ri, err := Get("refresh_interval")
 			if err != nil {
+				log.Println("'refresh' not set, unsing default :", default_refresh_interval)
 				ri = default_refresh_interval
 			}
-            ref_interval, ok := ri.(float64)
-            if !ok {
-                ref_interval = default_refresh_interval
-            }
+			ref_interval, ok := ri.(int)
+			if !ok {
+				log.Println("invalid 'refresh', unsing default :", default_refresh_interval)
+				ref_interval = default_refresh_interval
+			}
 			time.Sleep(time.Duration(ref_interval) * time.Second)
 		}
 	}()
 }
 
-type config struct {
-	sync.Mutex
-	data map[string]interface{}
-}
-
-func Get(key string) (interface{}, error) {
+func Get(keys ...string) (res interface{}, err error) {
 	c.Lock()
 	defer c.Unlock()
-	if val, ok := c.data[key]; ok {
-		return val, nil
+	res = c.data
+	for i := 0; i < len(keys); i++ {
+        t, ok := map[string]interface{}{}, false
+		if t, ok = res.(map[string]interface{}); !ok {
+			return nil, errors.New(fmt.Sprint("can't get vale for", keys))
+		}
+		if res, ok = t[keys[i]]; !ok {
+			return nil, errors.New(fmt.Sprint("can't get vale for", keys))
+		}
 	}
-	return nil, errors.New("no such value: " + key)
-}
-
-func Put(key string, val interface{}) error {
-	c.Lock()
-	defer c.Unlock()
-	c.data[key] = val
-	data, err := json.Marshal(c.data)
-	if err != nil {
-		return err
-	}
-	err = ioutil.WriteFile(cPath, data, 0666)
-	return err
-}
-
-func Del(key string) {
-	c.Lock()
-	defer c.Unlock()
-	delete(c.data, key)
+	return
 }
 
 func LoadConfig(path string) error {
 	if path == "" {
-		path = cPath
-	} else {
-		cPath = path
+		path = c.path
 	}
-	c.Lock()
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
+        log.Println("failed to read config file :", err)
 		return err
 	}
+	c.Lock()
 	defer c.Unlock()
-	if err := json.Unmarshal(data, &c.data); err != nil {
-		return err
-	}
-	return nil
+	c.path = path
+	return json.Unmarshal(data, &c.data)
 }

aeceb31fdc25461fe21ebb970c8589c4079f52bc
cron <cron@debcut.vb>
2012-08-03 23:07:48 +0300

    added documentation + small bug fix

--- conf.go
+++ conf.go
@@ -1,3 +1,16 @@
+/*
+Package goconf implements simple concurency-safe configuration tool.
+All configuration parameters are read from json encoded file.
+
+Default file name is "config.json", but can be changed easily (see LoadConfig)
+File is re-read periodically, with default interval of 5 seconds.
+It can be configured via "refresh" parameter in the same config file.
+
+Example config.json is located in the repository.
+
+Created as a helper library for my other projects, not tested well,
+use at your own risk, no guarantees!
+*/
 package goconf
 
 import (
@@ -29,7 +42,7 @@ func init() {
 	go func() {
 		for {
 			LoadConfig(c.path)
-			ri, err := Get("refresh_interval")
+			ri, err := Get("refresh")
 			if err != nil {
 				log.Println("'refresh' not set, unsing default :", default_refresh_interval)
 				ri = default_refresh_interval
@@ -44,6 +57,9 @@ func init() {
 	}()
 }
 
+//Function Get is used to retrieve values from config.
+//Can recieve multiple args in which case it tries to get deeper
+//into the config json structure.
 func Get(keys ...string) (res interface{}, err error) {
 	c.Lock()
 	defer c.Unlock()
@@ -60,6 +76,9 @@ func Get(keys ...string) (res interface{}, err error) {
 	return
 }
 
+//Function LoadConfig reloads configuration data from file named by path
+//path can be set to "" (empty string) in which case the last used file name
+//is kept. The last successive file is always used for auto-update.
 func LoadConfig(path string) error {
 	if path == "" {
 		path = c.path
@@ -71,6 +90,9 @@ func LoadConfig(path string) error {
 	}
 	c.Lock()
 	defer c.Unlock()
+    if err := json.Unmarshal(data, &c.data); err != nil {
+        return err
+    }
 	c.path = path
-	return json.Unmarshal(data, &c.data)
+    return nil
 }
